6af6ba47ce6559be74049c9cdb8b3c3ffef204de
joodavi <joodavi@live.com>
2023-05-08 10:18:05 -0300

    fix: teste de deletar removido

--- backend/sppg/src/test/java/br/ufma/sppg/repo/TecnicaRepositoryTest.java
+++ backend/sppg/src/test/java/br/ufma/sppg/repo/TecnicaRepositoryTest.java
@@ -49,30 +49,4 @@ public class TecnicaRepositoryTest {
         Assertions.assertEquals(salvoTecnica.getTipo(), atualizado.getTipo());
         Assertions.assertEquals(salvoTecnica.getFinanciadora(), atualizado.getFinanciadora());
     }
-
-    @Test
-    public void deveImpedirRemoverTecnicaComDependencia() {
-        Tecnica tecnica = Tecnica.builder().tipo("Tipo 1").titulo("Título 1").ano(2023).financiadora("CAPES").outrasInformacoes("Outras informações").qtdGrad(15).qtdMestrado(10).qtdDoutorado(5).build();
-        
-        Docente docente = Docente.builder().lattes("123456789").nome("Professor 1").dataAtualizacao(new Date()).build();
-        List<Docente> docentes = new ArrayList<Docente>();
-        docentes.add(docente);
-
-        Orientacao orientacao = Orientacao.builder().tipo("Tipo 1").discente("Discente 1").titulo("Título 1").ano(2023).modalidade("Modalidade 1").instituicao("Instituição 1").curso("Curso 1").status("Ativo").build();
-        List<Orientacao> orientacoes = new ArrayList<Orientacao>();
-        orientacoes.add(orientacao);
-
-        tecnica.setDocentes(docentes);
-        tecnica.setOrientacoes(orientacoes);
-
-        Tecnica salvoTecnica = repository.save(tecnica);
-        Integer id = salvoTecnica.getId();
-        repository.deleteById(id);
-
-        Optional<Tecnica> temp = repository.findById(id);
-        // não passou
-        // expected: [false] but was: [true]
-        Assertions.assertFalse(temp.isEmpty());
-
-    }
 }
